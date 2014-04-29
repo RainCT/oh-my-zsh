f83a0f1a09e4bc1592e8c8b57b5dbd6e511eef42
Siegfried-Angel Gevatter Pujals <siegfried@gevatter.com>
2014-04-29 22:43:23 +0200

    Alias "." to "source"

--- custom/setup.zsh
+++ custom/setup.zsh
@@ -9,3 +9,6 @@ setopt correct_all
 
 # Show CPU stats for commands taking longer than 10 seconds (once they finish)
 REPORTTIME=10
+
+# Not sure what "." is for in zsh, but make it sane again:
+alias .='source'
